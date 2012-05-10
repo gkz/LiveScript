e15012face75ad4bab0977f7e7640db56f0af9d5
George Zahariev <z@georgezahariev.com>
2012-05-10 14:56:30 -0700

    compiled less and ls for better page performance

---
+++ site.js
@@ -0,0 +1,77 @@
+(function(){
+  $(function(){
+    var example, src, websafe, boom, __i, __ref, __len;
+    for (__i = 0, __len = (__ref = $('.example .example-ls')).length; __i < __len; ++__i) {
+      example = __ref[__i];
+      src = $(example).find('.lang-ls').html();
+      $('<pre class="source"></pre>').html(src).appendTo(example);
+    }
+    prettyPrint();
+    websafe = function(txt){
+      return String(txt).split('&').join('&amp;').split('<').join('&lt;');
+    };
+    boom = function(action){
+      var source, func, error, result, toPrepend;
+      source = $('.compiler textarea').val();
+      result = (function(){
+        try {
+          func = action === 'run' ? 'compile' : action;
+          return LiveScript[func](source, {
+            bare: true
+          });
+        } catch (e) {
+          error = true;
+          return e.message;
+        }
+      }());
+      if (action === 'run') {
+        result = (function(){
+          try {
+            return eval(result);
+          } catch (e) {
+            error = true;
+            return e.message;
+          }
+        }());
+      }
+      if (result != null) {
+        if (typeof console != 'undefined' && console !== null) {
+          console.log(result);
+        }
+        if (action == 'lex' || action == 'tokens') {
+          result = JSON.stringify(result);
+        }
+        result = websafe(result);
+        result = result.replace(/\n/g, '<br>').replace(/\ /g, '&nbsp');
+        if (action === 'compile' && !error) {
+          result = prettyPrintOne(result, 'lang-js', false);
+        }
+        toPrepend = error
+          ? "<div>\n<h3>" + action + " - error<span class=\"close\" title=\"Close\" >&times;</span></h3>\n<div class=\"alert alert-error\">" + result + "</div>\n</div>"
+          : "<div>\n<h3>" + action + "<span class=\"close\" title=\"Close\" >&times;</span></h3>\n<pre class=\"prettyprint lang-js\">" + result + "</pre>\n</div>";
+        $(toPrepend).prependTo('.compiler-output').attr('title', source);
+      }
+    };
+    $('.compiler-output').on('click', '.close', function(){
+      $(this).parent().parent().hide();
+      return false;
+    });
+    $('.actions button').on('click', function(){
+      return boom($(this).data('action'));
+    });
+    $('.example').on('dblclick', function(){
+      return $('.compiler textarea').val($(this).find('.source').text());
+    });
+    $('.sidebar .nav').on('click', 'a', function(){
+      $('.nav li').removeClass('active');
+      return $(this).closest('li').addClass('active');
+    });
+    $('h1 a').click(function(){
+      $('.nav li').removeClass('active');
+      return $('.nav li').first().addClass('active');
+    });
+    return $('.sidebar .nav').scrollspy({
+      offset: 30
+    });
+  });
+}).call(this);
---
+++ site.ls
@@ -0,0 +1,65 @@
+$ ->
+  for example in $ '.example .example-ls'
+    src = $ example .find \.lang-ls .html!
+    $ '<pre class="source"></pre>' .html src .appendTo example
+
+  prettyPrint!
+
+  websafe = (txt) ->
+      # works better than replace, though perhaps not as fast
+      String txt .split '&' .join '&amp;' .split '<' .join '&lt;'
+
+  !boom(action) = 
+    source = $ '.compiler textarea' .val!
+    result = try 
+      func = if action is \run then \compile else action
+      LiveScript[func](source, {+bare})
+    catch e
+      error = true
+      e.message
+    if action is \run
+      result = try
+        eval result
+      catch e
+        error = true
+        e.message
+    if result?
+      console?.log result
+      result = JSON.stringify result if action in <[ lex tokens ]>
+      result = websafe result
+      result = result.replace /\n/g, '<br>' .replace /\ /g, \&nbsp;
+      if action is \compile and not error
+        result = prettyPrintOne result, 'lang-js', false
+
+      toPrepend = if error
+        """<div>
+        <h3>#action - error<span class="close" title="Close" >&times;</span></h3>
+        <div class="alert alert-error">#result</div>
+        </div>"""
+      else
+        """<div>
+        <h3>#action<span class="close" title="Close" >&times;</span></h3>
+        <pre class="prettyprint lang-js">#result</pre>
+        </div>"""
+
+      $ toPrepend .prependTo '.compiler-output' .attr \title, source
+
+  $ '.compiler-output' .on \click \.close ->
+    $(this) .parent! .parent! .hide!   # or remove()
+    return false
+
+  $ '.actions button' .on \click ->
+    boom($ @ .data \action)
+
+  $ '.example' .on \dblclick ->
+    $ '.compiler textarea' .val($ this .find '.source' .text!)
+
+  $ '.sidebar .nav'  .on \click \a ->
+    $ '.nav li' .removeClass \active
+    $ this .closest \li .addClass \active
+
+  $ 'h1 a' .click ->
+    $ '.nav li' .removeClass \active
+    $ '.nav li' .first!.addClass \active
+
+  $ '.sidebar .nav' .scrollspy offset: 30
