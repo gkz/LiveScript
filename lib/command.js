gkz/LiveScript

commit 03c6e6a907a253b5cc6750a8721f03079307b540

remove process.argv mutation
Fix gkz/LiveScript#916. See that issue for commentary.

lib/command.js
@@ -11,7 +11,7 @@ version = LiveScript.VERSION;
 (function(it){
   return module.exports = it;
 })(function(args, arg$){
-  var ref$, say, sayWithTimestamp, warn, die, o, positional, e, validMapValues, toInsert, that, filename, jsonCallback;
+  var ref$, say, sayWithTimestamp, warn, die, o, positional, e, validMapValues, that, filename, jsonCallback;
   ref$ = arg$ != null
     ? arg$
     : {}, say = ref$.say, sayWithTimestamp = ref$.sayWithTimestamp, warn = ref$.warn, die = ref$.die;
@@ -52,15 +52,6 @@ version = LiveScript.VERSION;
       }
     } else {
       o.map = o.run || o.eval ? 'embedded' : 'none';
-    }
-    if (args === process.argv) {
-      process.argv[0] = process.argv[1];
-      toInsert = o.stdin
-        ? positional
-        : o.run
-          ? positional.splice(1, 9e9)
-          : [];
-      (ref$ = process.argv).splice.apply(ref$, [2, 9e9].concat(slice$.call(toInsert)));
     }
     if (that = o.require) {
       filename = module.filename;

src/command.ls
@@ -37,14 +37,6 @@ switch
       die "Option --map must be either: #{ valid-map-values.join ', ' }"
   else o.map = if o.run or o.eval then 'embedded' else 'none'
 
-  if args is process.argv
-    process.argv.0 = process.argv.1
-    to-insert = if o.stdin
-      positional
-    else
-      if o.run then positional.splice 1 9e9 else []
-    process.argv.splice 2, 9e9, ...to-insert
-
   if o.require
     {filename} = module
     module.filename = '.'

lib/node.js
@@ -13,11 +13,9 @@ module.exports = function(LiveScript){
       ? arg$
       : {}, js = ref$.js, context = ref$.context;
     main = require.main;
-    if (filename) {
-      dirname = path.dirname(fs.realpathSync(filename = process.argv[1] = path.resolve(filename)));
-    } else {
-      dirname = filename = '.';
-    }
+    dirname = filename
+      ? path.dirname(fs.realpathSync(filename = path.resolve(filename)))
+      : filename = '.';
     main.paths = main.constructor._nodeModulePaths(dirname);
     main.filename = filename;
     if (!js) {

src/node.ls
@@ -9,11 +9,10 @@ module.exports = !(LiveScript) ->
     LiveScript.run = (code, {filename}:options?, {js, context} = {}) ->
         {main} = require
         # Hack for relative `require`.
-        if filename
-            dirname = path.dirname fs.realpath-sync do
-                filename = process.argv.1 = path.resolve filename
+        dirname = if filename
+            path.dirname fs.realpath-sync filename |>= path.resolve
         else
-            dirname = filename = '.'
+            filename = '.'
         main.paths = main.constructor._node-module-paths dirname
         main <<< {filename}
         unless js

commit 5ab06f740484d97776aa2a324185a54f9640e839

fix script argument regression
In commit 03c6e6a, removing the code that mutated process.argv also
removed a side effect that prevented all arguments to lsc from being
handled as scripts to compile and run. This caused a regression when
running `lsc script-file.ls arg1 arg2` that would result in a crash when
no `arg1.ls` file could be found.

The regression is fixed by reintroducing the side effect to remove all
but the first positional argument from the array containing the scripts
to compile and run, but only when lsc is running scripts--batch
compilation commands involving multiple input files should remain
unaffected.

This commit also adds a `process.argv.lsc` property, present only when a
script is being interpreted by lsc, which is an array holding the name
of the script being invoked along with any extra arguments not consumed
by lsc. The intent here is to provide a way to get script arguments
that's simpler and more reliable than picking through process.argv and
skipping over elements like node, the lsc compiler itself, and any CLI
options being provided to either.

lib/command.js
@@ -1,5 +1,5 @@
 // Generated by LiveScript 1.5.0
-var LiveScript, path, fs, util, prelude, each, breakList, ref$, parseOptions, generateHelp, nameFromPath, version, this$ = this, slice$ = [].slice;
+var LiveScript, path, fs, util, prelude, each, breakList, ref$, parseOptions, generateHelp, nameFromPath, version, this$ = this, slice$ = [].slice, arrayFrom$ = Array.from || function(x){return slice$.call(x);};
 LiveScript = require('..');
 path = require('path');
 fs = require('fs');
@@ -52,6 +52,11 @@ version = LiveScript.VERSION;
       }
     } else {
       o.map = o.run || o.eval ? 'embedded' : 'none';
+    }
+    if (o.run && args === process.argv) {
+      process.argv.lsc = o.stdin || o.eval
+        ? [void 8].concat(arrayFrom$(positional))
+        : [positional[0]].concat(arrayFrom$(positional.splice(1)));
     }
     if (that = o.require) {
       filename = module.filename;

src/command.ls
@@ -37,6 +37,15 @@ switch
       die "Option --map must be either: #{ valid-map-values.join ', ' }"
   else o.map = if o.run or o.eval then 'embedded' else 'none'
 
+  if o.run and args is process.argv
+    process.argv.lsc =
+      if o.stdin or o.eval
+        [void ...positional]
+      else
+        # If running a script, this splice keeps arguments after the first from
+        # being handled as additional sources to be compiled.
+        [positional.0, ...positional.splice 1]
+
   if o.require
     {filename} = module
     module.filename = '.'