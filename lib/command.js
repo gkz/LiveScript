gkz/LiveScript

commit 68a6d215e790eccb1047af0b7c91f9b695adadc6

Fix flag combinations: -ce and -aj
Fixes #993

lib/command.js
@@ -74,7 +74,7 @@ version = LiveScript.VERSION;
       if (positional.length && (o.json || /\.json$/.test(positional[0]))) {
         o.json = true;
         fshoot('readFile', positional[0], jsonCallback);
-      } else if (o.json) {
+      } else if (o.json && !o.ast) {
         getStdin(jsonCallback);
       } else {
         compileScript('', o.eval);
@@ -173,7 +173,7 @@ version = LiveScript.VERSION;
         throw null;
       }
       json = o.json || /\.json\.ls$/.test(filename);
-      run = o.run || o.eval;
+      run = o.run || json;
       if (run) {
         LiveScript.emit('compile', t);
         print = json || o.print;

src/command.ls
@@ -52,7 +52,7 @@ switch
       if positional.length and (o.json or /\.json$/.test positional.0)
           o.json = true
           fshoot 'readFile', positional.0, json-callback
-      else if o.json
+      else if o.json and not o.ast
           get-stdin json-callback
       else
           compile-script '' o.eval
@@ -119,7 +119,7 @@ switch
       throw
 
     json = o.json or /\.json\.ls$/.test filename
-    run = o.run or o.eval
+    run = o.run or json
     if run
       LiveScript.emit 'compile' t
       print = json or o.print

commit 5ac627d55cf6f751f293d548e2bfcdcf29b3c717

stop embedding source map with -ce
68a6d21 introduced a regression in the functionality of 9d32954 by
causing a source map to be embedded by default when using the -ce
combination of compiler flags, in contradiction with the comment of
9d32954 which states that embedded source maps should only be the
default when the compiler is running, not just compiling, code. With
68a6d21, the presence of `o.eval` no longer forces the compiler to run
code even if -c is used, so `o.eval` is no longer relevant to the
default value of `o.map`.

lib/command.js
@@ -51,7 +51,7 @@ version = LiveScript.VERSION;
         die("Option --map must be either: " + validMapValues.join(', '));
       }
     } else {
-      o.map = o.run || o.eval ? 'embedded' : 'none';
+      o.map = o.run ? 'embedded' : 'none';
     }
     if (that = o.require) {
       filename = module.filename;

src/command.ls
@@ -35,7 +35,7 @@ switch
     valid-map-values = <[ none linked linked-src embedded debug ]>
     if o.map not in valid-map-values
       die "Option --map must be either: #{ valid-map-values.join ', ' }"
-  else o.map = if o.run or o.eval then 'embedded' else 'none'
+  else o.map = if o.run then 'embedded' else 'none'
 
   if o.require
     {filename} = module