gkz/LiveScript

commit 41fb1a8e0305d7e6dc403ffb4512595742c0f5e8

fix shebang and header ordering
Earlier support for shebang lines in the compiled output moved them
before the default function wrapper, but not before the default
LiveScript header comment. This moves shebangs above the header comment
as well.

lib/ast.js
@@ -688,7 +688,7 @@ exports.Block = Block = (function(superclass){
     return sn.apply(null, [null].concat(arrayFrom$(codes)));
   };
   Block.prototype.compileRoot = function(options){
-    var o, that, ref$, bare, prefix, ref1$, code, result;
+    var o, that, ref$, bare, prefix, ref1$, code;
     o = (import$({
       level: LEVEL_TOP,
       scope: this.scope = Scope.root = new Scope
@@ -710,7 +710,7 @@ exports.Block = Block = (function(superclass){
     }
     code = [this.compileWithDeclarations(o)];
     bare || (code = ["(function(){\n"].concat(arrayFrom$(code), ["\n}).call(this);\n"]));
-    return result = sn.apply(null, [null, prefix || []].concat(arrayFrom$(code)));
+    return sn(null, prefix || [], options.header || [], code);
   };
   Block.prototype.compileWithDeclarations = function(o){
     var pre, i, rest, post, that;

src/ast.ls
@@ -480,7 +480,7 @@ class exports.Block extends Node
         code = [(@compile-with-declarations o)]
         # Wrap everything in a safety closure unless requested not to.
         bare or code = ["(function(){\n", ...code, "\n}).call(this);\n"]
-        result = sn(null, prefix || [], ...code)
+        sn null, prefix || [], options.header || [], code
 
     # Compile to a function body.
     compile-with-declarations: (o) ->

test/compilation.ls
@@ -372,3 +372,11 @@ LiveScript.compile '''
     d
       .e (.f in [2 3])
 '''
+
+# [LiveScript#1030](https://github.com/gkz/LiveScript/issues/1030)
+compiled = LiveScript.compile '''
+#!lsc shebang line
+``#!node shebang line``
+foo bar
+''' {+header}
+ok compiled.starts-with '#!node shebang line\n'

lib/index.js
@@ -33,6 +33,9 @@ exports.compile = function(code, options){
   var result, ast, output, filename, outputFilename, ref$, mapPath, e, that;
   options == null && (options = {});
   options.header == null && (options.header = true);
+  if (options.header === true) {
+    options.header = "// Generated by LiveScript " + exports.VERSION + "\n";
+  }
   try {
     if (options.json) {
       result = Function(exports.compile(code, {
@@ -47,9 +50,6 @@ exports.compile = function(code, options){
         ast.makeReturn();
       }
       output = ast.compileRoot(options);
-      if (options.header) {
-        output = new SourceNode(null, null, null, ["// Generated by LiveScript " + exports.VERSION + "\n", output]);
-      }
       if (options.map && options.map !== 'none') {
         filename = options.filename, outputFilename = options.outputFilename;
         if (!filename) {

src/index.ls
@@ -32,6 +32,7 @@ exports <<<
     # Compiles a string of LiveScript code to JavaScript.
     compile: (code, options = {}) ->
       options.header ?= true
+      options.header = "// Generated by LiveScript #{exports.VERSION}\n" if options.header is true
       try
           if options.json
               result = do Function exports.compile code, {+bare, +run, +print}
@@ -40,10 +41,6 @@ exports <<<
               ast = parser.parse lexer.lex code
               ast.make-return! if options.run and options.print
               output = ast.compile-root options
-              if options.header
-                  output = new SourceNode null, null, null, [
-                      "// Generated by LiveScript #{exports.VERSION}\n", output
-                  ]
               if options.map and options.map isnt 'none'
                   {filename, output-filename} = options
                   unless filename

commit a6740904078c47be541c9ac31cee03c5af03d13b

fix regression caused by 41fb1a8
That attempt to move shebang lines above the header comment also moved
initial source comments above the header comment, which was unintended.
If any of these three elements are present, the correct order is:

  * shebang
  * Generated by LiveScript header comment
  * source file header comment

lib/ast.js
@@ -694,7 +694,7 @@ exports.Block = Block = (function(superclass){
     return sn.apply(null, [null].concat(arrayFrom$(codes)));
   };
   Block.prototype.compileRoot = function(options){
-    var o, that, ref$, bare, prefix, ref1$, code;
+    var o, that, ref$, bare, prefix, ref1$, ref2$, comment, ref3$, code;
     o = (import$({
       level: LEVEL_TOP,
       scope: this.scope = Scope.root = new Scope
@@ -704,10 +704,13 @@ exports.Block = Block = (function(superclass){
     }
     delete o.filename;
     o.indent = (bare = (ref$ = o.bare, delete o.bare, ref$)) ? '' : TAB;
-    if (/^\s*(?:[/#]|javascript:)/.test((ref$ = this.lines[0]) != null ? ref$.code : void 8)) {
+    if (/^\s*(?:#!|javascript:)/.test((ref$ = this.lines[0]) != null ? ref$.code : void 8)) {
       prefix = this.lines.shift().code + '\n';
     }
-    if ((ref1$ = o.eval, delete o.eval, ref1$) && this.chomp().lines.length) {
+    if (((ref1$ = this.lines[0]) != null ? (ref2$ = ref1$.code) != null ? ref2$[0] : void 8 : void 8) === '/') {
+      comment = this.lines.shift().code + '\n';
+    }
+    if ((ref3$ = o.eval, delete o.eval, ref3$) && this.chomp().lines.length) {
       if (bare) {
         this.lines.push(Parens(this.lines.pop()));
       } else {
@@ -716,7 +719,7 @@ exports.Block = Block = (function(superclass){
     }
     code = [this.compileWithDeclarations(o)];
     bare || (code = ["(function(){\n"].concat(arrayFrom$(code), ["\n}).call(this);\n"]));
-    return sn(null, prefix || [], options.header || [], code);
+    return sn(null, prefix || [], options.header || [], comment || [], code);
   };
   Block.prototype.compileWithDeclarations = function(o){
     var pre, i, rest, post, that;

src/ast.ls
@@ -480,14 +480,16 @@ class exports.Block extends Node
             @scope = Scope.root = o.scope = that.savedScope or= o.scope
         delete o.filename
         o.indent = if bare = delete o.bare then '' else TAB
-        if /^\s*(?:[/#]|javascript:)/test @lines.0?.code
+        if /^\s*(?:#!|javascript:)/test @lines.0?code
             prefix = @lines.shift!code + \\n
+        if @lines.0?code?0 is '/'
+            comment = @lines.shift!code + \\n
         if delete o.eval and @chomp!lines.length
             if bare then @lines.push Parens @lines.pop! else @make-return!
         code = [(@compile-with-declarations o)]
         # Wrap everything in a safety closure unless requested not to.
         bare or code = ["(function(){\n", ...code, "\n}).call(this);\n"]
-        sn null, prefix || [], options.header || [], code
+        sn null, prefix || [], options.header || [], comment || [], code
 
     # Compile to a function body.
     compile-with-declarations: (o) ->

test/compilation.ls
@@ -377,9 +377,13 @@ LiveScript.compile '''
 compiled = LiveScript.compile '''
 #!lsc shebang line
 ``#!node shebang line``
+/* Copyright Shmorkum Porkums, 20XX */
 foo bar
-''' {+header}
-ok compiled.starts-with '#!node shebang line\n'
+''' {-bare,+header} .split '\n'
+eq compiled.0, '#!node shebang line'
+ok compiled.1.starts-with '// Generated by LiveScript'
+eq compiled.2, '/* Copyright Shmorkum Porkums, 20XX */'
+ok compiled.3.starts-with '(function'
 
 # Reuse reference variables when appropriate
 compiled = LiveScript.compile 'a.b[*-1].=c' {+bare,-header}