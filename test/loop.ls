gkz/LiveScript

commit 0e420e01f3f17e17aba5a1e31607ea29f90e9f85

fix loop guards + let + destructuring issues
Hat tip to vendethiel for reporting and writing tests.

lib/ast.js
@@ -891,8 +891,8 @@ exports.Var = Var = (function(superclass){
     return this$;
   } function ctor$(){} ctor$.prototype = prototype;
   prototype.isAssignable = prototype.isCallable = YES;
-  Var.prototype.assigns = function(it){
-    return it === this.value;
+  Var.prototype.assigns = function(){
+    return [this.value];
   };
   Var.prototype.maybeKey = function(){
     var ref$;
@@ -915,8 +915,8 @@ exports.Key = Key = (function(superclass){
     return this$;
   } function ctor$(){} ctor$.prototype = prototype;
   Key.prototype.isComplex = NO;
-  Key.prototype.assigns = function(it){
-    return it === this.name;
+  Key.prototype.assigns = function(){
+    return [this.name];
   };
   Key.prototype.maybeKey = THIS;
   Key.prototype.varName = function(){
@@ -1563,14 +1563,23 @@ List = (function(superclass){
   List.prototype.isEmpty = function(){
     return !this.items.length;
   };
-  List.prototype.assigns = function(it){
-    var i$, ref$, len$, node;
+  List.prototype.assigns = function(){
+    var x$, res$, i$, ref$, len$, node, that, j$, len1$, x;
+    res$ = [];
     for (i$ = 0, len$ = (ref$ = this.items).length; i$ < len$; ++i$) {
       node = ref$[i$];
-      if (node.assigns(it)) {
-        return true;
+      if (that = node.assigns()) {
+        for (j$ = 0, len1$ = that.length; j$ < len1$; ++j$) {
+          x = that[j$];
+          res$.push(x);
+        }
       }
     }
+    x$ = res$;
+    if (that = this.name) {
+      x$.push(that);
+    }
+    return x$;
   };
   List.compile = function(o, items, deepEq){
     var indent, level, code, i, that, target;
@@ -1755,9 +1764,9 @@ exports.Prop = Prop = (function(superclass){
   Prop.prototype.show = function(){
     return this.accessor;
   };
-  Prop.prototype.assigns = function(it){
+  Prop.prototype.assigns = function(){
     var ref$;
-    return typeof (ref$ = this.val).assigns == 'function' ? ref$.assigns(it) : void 8;
+    return typeof (ref$ = this.val).assigns == 'function' ? ref$.assigns() : void 8;
   };
   Prop.prototype.compileAccessor = function(o, key){
     var funs, code, i$, len$, fun;
@@ -2494,8 +2503,8 @@ exports.Assign = Assign = (function(superclass){
   Assign.prototype.show = function(){
     return [void 8].concat(this.unaries).reverse().join(' ') + [this.logic] + this.op;
   };
-  Assign.prototype.assigns = function(it){
-    return this.left.assigns(it);
+  Assign.prototype.assigns = function(){
+    return this.left.assigns();
   };
   prototype.delegate(['isCallable', 'isRegex'], function(it){
     var ref$;
@@ -2645,7 +2654,7 @@ exports.Assign = Assign = (function(superclass){
     if (that = left.name) {
       cache = sn(this, that, " = ", rite);
       o.scope.declare(rite = that, left);
-    } else if ((ret || len > 1) && (!ID.test(rite.toString()) || left.assigns(rite.toString()))) {
+    } else if ((ret || len > 1) && (!ID.test(rite.toString()) || ((that = left.assigns()) && in$(rite.toString(), that)))) {
       cache = sn(this, rref = o.scope.temporary(), " = ", rite);
       rite = rref;
     }
@@ -3497,8 +3506,8 @@ exports.Splat = Splat = (function(superclass){
   } function ctor$(){} ctor$.prototype = prototype;
   ref$ = Parens.prototype, prototype.children = ref$.children, prototype.isComplex = ref$.isComplex;
   Splat.prototype.isAssignable = YES;
-  Splat.prototype.assigns = function(it){
-    return this.it.assigns(it);
+  Splat.prototype.assigns = function(){
+    return this.it.assigns();
   };
   Splat.prototype.compile = function(){
     return this.carp('invalid splat');
@@ -3851,7 +3860,7 @@ exports.For = For = (function(superclass){
     return ((this.kind || []).concat(this.index)).join(' ');
   };
   For.prototype.addBody = function(body){
-    var hasYield, ref$, x$, that, this$ = this;
+    var hasYield, ref$, that, assignments, x$, assigned, name, ref1$;
     hasYield = !!body.traverseChildren(function(child){
       if (child instanceof Yield) {
         return true;
@@ -3861,21 +3870,27 @@ exports.For = For = (function(superclass){
       if (ref$ = this.ref, delete this.ref, ref$) {
         this.item = Literal('..');
       }
-      body = Block(Call['let']((x$ = [], (that = this.index) && x$.push(Assign(Var(that), Literal('index$$'))), (that = this.item) && x$.push(Assign(that, Literal('item$$'))), x$), body, hasYield));
+      if (that = (ref$ = this.item) != null ? ref$.rewriteShorthand() : void 8) {
+        this.item = that;
+      }
+      assignments = (x$ = [], (that = this.index) && x$.push(Assign(Var(that), Literal('index$$'))), (that = this.item) && x$.push(Assign(that, Literal('item$$'))), x$);
+      body = Block(this.guard
+        ? (assigned = (function(){
+          var i$, x$, ref$, len$, j$, ref1$, len1$, results$ = [];
+          for (i$ = 0, len$ = (ref$ = assignments).length; i$ < len$; ++i$) {
+            x$ = ref$[i$];
+            if (that = x$.assigns()) {
+              for (j$ = 0, len1$ = (ref1$ = that).length; j$ < len1$; ++j$) {
+                name = ref1$[j$];
+                results$.push(Var(name));
+              }
+            }
+          }
+          return results$;
+        }()), assignments.concat([If((ref1$ = this.guard, delete this.guard, ref1$), Call['let'](assigned, body, hasYield))]))
+        : Call['let'](assignments, body, hasYield));
     }
     superclass.prototype.addBody.call(this, body);
-    if (this.guard && this['let'] && (this.index || this.item)) {
-      this.body.lines[0]['if'].traverseChildren(function(it){
-        if (it instanceof Var) {
-          if (this$.index && it.value === this$.index) {
-            it.value = 'index$$';
-          }
-          if (this$.item && it.value === this$.item.value) {
-            it.value = 'item$$';
-          }
-        }
-      });
-    }
     if (this['let']) {
       if (hasYield) {
         this.body = Block(Yield('yieldfrom', body));

src/ast.ls
@@ -285,7 +285,7 @@ SourceNode::to-string = (...args) ->
 
     is-matcher: -> @is-string! or @is-regex!
 
-    # Do I assign a certain variable?
+    # Do I assign any variables? (Returns a non-empty array if so.)
     assigns: NO
 
     # Picks up name(s) from LHS.
@@ -576,7 +576,7 @@ class exports.Var extends Atom
 
     ::is-assignable = ::is-callable = YES
 
-    assigns: -> it is @value
+    assigns: -> [@value]
 
     maybe-key: -> Key(@value) <<< {@line}
 
@@ -591,7 +591,7 @@ class exports.Key extends Node
 
     is-complex: NO
 
-    assigns: -> it is @name
+    assigns: -> [@name]
 
     maybe-key: THIS
 
@@ -1005,7 +1005,8 @@ class List extends Node
     named : (@name) -> this
 
     is-empty: -> not @items.length
-    assigns: -> for node in @items then return true if node.assigns it
+    assigns: -> with [x for node in @items when node.assigns! for x in that]
+        ..push that if @name
 
     @compile = (o, items, deep-eq) ->
         switch items.length
@@ -1118,7 +1119,7 @@ class exports.Prop extends Node
 
     show: -> @accessor
 
-    assigns: -> @val.assigns? it
+    assigns: -> @val.assigns?!
 
     compile-accessor: (o, key) ->
         funs = @val
@@ -1564,7 +1565,7 @@ class exports.Assign extends Node
 
     show: -> [,]concat(@unaries)reverse!join(' ') + [@logic] + @op
 
-    assigns: -> @left.assigns it
+    assigns: -> @left.assigns!
 
     ::delegate <[ isCallable isRegex ]> -> @op in <[ = := ]> and @right and @right[it]!
 
@@ -1672,7 +1673,7 @@ class exports.Assign extends Node
         if left.name
             cache = sn(this, that, " = ", rite)
             o.scope.declare rite = that, left
-        else if (ret or len > 1) and (not ID.test rite.to-string! or left.assigns rite.to-string!)
+        else if (ret or len > 1) and (not ID.test rite.to-string! or if left.assigns! then rite.to-string! in that)
             cache = sn(this, (rref = o.scope.temporary!), " = ", rite)
             rite  = rref
         if rite.to-string! is \arguments and not ret
@@ -2246,7 +2247,7 @@ class exports.Splat extends Node
 
     is-assignable: YES
 
-    assigns: -> @it.assigns it
+    assigns: -> @it.assigns!
 
     compile: -> @carp 'invalid splat'
 
@@ -2465,22 +2466,18 @@ class exports.For extends While
             return true if child instanceof Yield
         if @let
             @item = Literal \.. if delete @ref
-            body = Block Call.let do
-                with []
-                    ..push Assign Var(that), Literal \index$$ if @index
-                    ..push Assign that,      Literal \item$$ if @item
-                body
-                has-yield
+            @item = that if @item?rewrite-shorthand!
+            assignments = with []
+                ..push Assign Var(that), Literal \index$$ if @index
+                ..push Assign that,      Literal \item$$ if @item
+            body = Block if @guard
+                assigned = [Var name for assignments when ..assigns! for name in that]
+                assignments.concat [If delete @guard, Call.let assigned, body, has-yield]
+            else
+                Call.let assignments, body, has-yield
 
         super body
 
-        if @guard and @let and (@index or @item)
-            @body.lines[0].if.traverse-children !~>
-                if it instanceof Var
-                    if @index and it.value is @index
-                        it.value = \index$$
-                    if @item and it.value is @item.value
-                        it.value = \item$$
         if @let
             @body := Block Yield \yieldfrom, body if has-yield
             delete @index

test/loop.ls
@@ -694,6 +694,43 @@ o = { [k, -> v] for let k, v of {a: 1, b: 2} }
 eq 1 o.a!
 eq 2 o.b!
 
+# interactions of guards+let, see #992
+arr = [0,1,2,3]
+r = []
+for let k in arr when k
+  r.push k
+eq "1,2,3", r.join ','
+
+# interaction of guards+let, when destructuring is used, see #992
+arr =
+  * letter: 'a' valueOf: -> 0
+  * letter: 'b' valueOf: -> 1
+  * letter: 'c' valueOf: -> 2
+r = []
+for let {letter}:guard-o in arr when guard-o > 0
+  r.push letter
+eq "b,c", r.join ','
+
+r = []
+for let {letter, valueOf} in arr when valueOf! > 0
+  r.push letter
+eq "b,c", r.join ','
+
+# more of the above, with extra nesting and complications
+arr =
+  * [true  {b: \alpha x: 0} {d: 1}]
+  * [false {b: \bravo}      {d: 2}]
+  * [true  {}               {d: 3}]
+  * [true  {b: \delta}      {d: 0}]
+  * [true  {b: false}       {d: 5}]
+fns = for let [a, {b ? \default}:c, {d: e}] in arr when a and b and e
+  -> {b, c, e}
+r = for f in fns then f!
+expected =
+  * b: \alpha c: {b: \alpha x: 0} e: 1
+  * b: \default c: {} e: 3
+deep-equal expected, r
+
 # Certain literals could result in illegal JavaScript if not carefully
 # handled. These are all nonsensical use cases and could just as easily
 # be LiveScript syntax errors. The thing to avoid is for them to be JavaScript

commit a50cd694583b579774855a7eb7e682d1ef6e9637

fix for-let destructuring regression
In commit 0e420e0, I have no idea why I thought it would be a good idea
to introduce a call to `rewrite-shorthand` with no arguments. It appears
to have been a kludge to make destructuring with defaults work, but
this is a better way to do that.

lib/ast.js
@@ -2241,6 +2241,10 @@ exports.Binary = Binary = (function(superclass){
       return this;
     }
   };
+  Binary.prototype.assigns = function(){
+    var ref$;
+    return (ref$ = this.getDefault()) != null ? ref$.first.assigns() : void 8;
+  };
   Binary.prototype.xorChildren = function(test){
     var ref$, first;
     if (!(!(first = test(this.first)) !== !(ref$ = test(this.second)) && (first || ref$))) {
@@ -3982,13 +3986,10 @@ exports.For = For = (function(superclass){
     return ((this.kind || []).concat(this.index)).join(' ');
   };
   For.prototype.addBody = function(body){
-    var ref$, that, assignments, x$, assigned, name, ref1$;
+    var ref$, assignments, x$, that, assigned, name;
     if (this['let']) {
       if (ref$ = this.ref, delete this.ref, ref$) {
         this.item = Literal('..');
-      }
-      if (that = (ref$ = this.item) != null ? ref$.rewriteShorthand() : void 8) {
-        this.item = that;
       }
       assignments = (x$ = [], (that = this.index) && x$.push(Assign(Var(that), Literal('index$$'))), (that = this.item) && x$.push(Assign(that, Literal('item$$'))), x$);
       body = Block(this.guard
@@ -4004,7 +4005,7 @@ exports.For = For = (function(superclass){
             }
           }
           return results$;
-        }()), assignments.concat([If((ref1$ = this.guard, delete this.guard, ref1$), Call['let'](assigned, body))]))
+        }()), assignments.concat([If((ref$ = this.guard, delete this.guard, ref$), Call['let'](assigned, body))]))
         : Call['let'](assignments, body));
     }
     superclass.prototype.addBody.call(this, body);

src/ast.ls
@@ -1398,6 +1398,8 @@ class exports.Binary extends Node
 
     get-default: -> switch @op | \? \|| \&& => this
 
+    assigns: -> @get-default!?first.assigns!
+
     xor-children: (test) ->
         return false unless (first = test @first) xor test @second
         return if first then [@first, @second] else [@second, @first]
@@ -2536,7 +2538,6 @@ class exports.For extends While
     add-body: (body) ->
         if @let
             @item = Literal \.. if delete @ref
-            @item = that if @item?rewrite-shorthand!
             assignments = with []
                 ..push Assign Var(that), Literal \index$$ if @index
                 ..push Assign that,      Literal \item$$ if @item

test/loop.ls
@@ -731,6 +731,22 @@ expected =
   * b: \default c: {} e: 3
 deep-equal expected, r
 
+# Unreported for-let regression
+arr =
+  * x: 6 y: 7
+  * x: 8 y: 9
+o = x: \x y: \y
+i = 0
+f = -> i++; o
+fns = for let {(f!x), (f!y)} in arr
+  t = o{x, y}
+  o.x = \x
+  o.y = \y
+  -> t
+r = for f in fns then f!
+deep-equal arr, r
+eq 4 i
+
 # Certain literals could result in illegal JavaScript if not carefully
 # handled. These are all nonsensical use cases and could just as easily
 # be LiveScript syntax errors. The thing to avoid is for them to be JavaScript